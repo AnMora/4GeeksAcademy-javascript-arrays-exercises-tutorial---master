f6e7f77644f4b842e90306dd8049ce9629bb9c33
Anmora96 <anmora.2396@gmail.com>
2021-01-24 06:31:16 +0000

    Yasirve ejercicio 3

--- exercises/03.1-Access-and-retrieve/app.js
+++ exercises/03.1-Access-and-retrieve/app.js
@@ -4,7 +4,7 @@ var myArray = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday',
 //1. print the item here
 console.log(myArray[2]);
 //2. change 'thursday'a value here to null
-var valornull = null;
-myArray.push([ 'thursday', valornull ]);
+var valornull = myArray.indexOf('thursday');
+myArray[valornull] = null;
 //3. print the position of step 2
 console.log(myArray[4]);
