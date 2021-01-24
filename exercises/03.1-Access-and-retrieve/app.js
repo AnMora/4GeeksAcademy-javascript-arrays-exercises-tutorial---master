b18ed4dd5c983761e54f9001e735ad286523147d
Anmora96 <anmora.2396@gmail.com>
2021-01-24 06:17:48 +0000

    Ejercicio 3

--- exercises/03.1-Access-and-retrieve/app.js
+++ exercises/03.1-Access-and-retrieve/app.js
@@ -1,9 +1,10 @@
 //declaring the array
-var myArray = ['sunday', 'monday', 'tuesday', 'wednesday', null, 'friday', 'saturday'];
+var myArray = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
 //1. print the item here
-console.log(myArray.length);
+console.log(myArray[2]);
 //2. change 'thursday'a value here to null
-console.log(myArray[4]);
+var valornull = null;
+myArray.push([ 'thursday', valornull ]);
 //3. print the position of step 2
-console.log(myArray[3]);
+console.log(myArray[4]);
