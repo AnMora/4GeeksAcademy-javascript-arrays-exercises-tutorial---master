08e5c614e47c09485422095e067922708b98d21c
Anmora96 <anmora.2396@gmail.com>
2021-01-25 04:17:52 +0000

    Ejercicio 5, compresion

--- exercises/05.3-Add-items-to-array/app.js
+++ exercises/05.3-Add-items-to-array/app.js
@@ -1,5 +1,12 @@
 var arr = [4,5,734,43,45];
 
 // Your code here, use the push function and the random function.
+function generateArray(arr){
+    var auxArray = arr;
+    var randomLength = Math.floor(Math.random()*100);
+    for(var i = 0;i<randomLength;i++)
+        auxArray.push(Math.floor(Math.random()*100));
+    return auxArray;
+}
 
 console.log(arr);
