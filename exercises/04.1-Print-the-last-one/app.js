86e34d0882d848b9e6a85edbda0fd0229f98480f
Anmora96 <anmora.2396@gmail.com>
2021-01-25 03:00:45 +0000

    Ejercicio 6

--- exercises/04.1-Print-the-last-one/app.js
+++ exercises/04.1-Print-the-last-one/app.js
@@ -8,4 +8,5 @@ var myStupidArray = generateRandomArray();
 
 //Your code here
 var theLastOne = myStupidArray[myStupidArray.length-1];
+//Se obtiene con lenght el ultimo elemento y el -1 se se posiciona en ese ultimo y lo muestra
 console.log(theLastOne);
